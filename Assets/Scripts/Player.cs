adf3ecba20b9fe907917e18b7167e73ec7b6f8e4
BATHTUBGERALT1 <43067557+BATHTUBGERALT1@users.noreply.github.com>
2018-10-26 16:19:28 +0100

    added player movement/direction animation

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    // designer variables
+    public float speed = 10;
+    public Rigidbody2D physicsBody;
+    public string horizontalAxis = "Horizontal";
+
+    public Animator playerAnimator;
+    public SpriteRenderer playerSprite;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // get axis input from unity 
+        float leftRight = Input.GetAxis(horizontalAxis);
+        
+
+        // Create direction vector from axis input
+        Vector2 direction = new Vector2(leftRight, 0);
+
+        // make direction vector length 1
+        direction = direction.normalized;
+
+        // calculate velocity 
+        Vector2 velocity = direction * speed;
+        velocity.y = physicsBody.velocity.y;
+
+        // give the velocity to the rigidbody
+        physicsBody.velocity = velocity;
+
+        //tell the animator our speed
+        playerAnimator.SetFloat("WalkSpeed", Mathf.Abs(velocity.x)); 
+
+        // flip sprite if we are moving backwards
+        if (velocity.x < 0)
+        {
+            playerSprite.flipX = true;
+        }
+        else
+        {
+            playerSprite.flipX = false;
+        }
+    }
+
+}
